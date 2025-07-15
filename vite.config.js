8540bf9044013bdeb8fafe798a57fd6e68ea6c0e
Rahul-Sandeep03 <rahulsandeep03@gmail.com>
2025-07-15 17:15:30 +0530

    Configure Vite base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const REPO_NAME = 'dance-studio';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dance-studio/',    // must be your repo name with slashes
+    base: `/${REPO_NAME}/`,      // must be your repo name with slashes
   plugins: [react()],
 });
